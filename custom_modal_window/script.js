2a557004264fe62b548a7c8c1651748414d95433
Max <82365842+a1exanders0n@users.noreply.github.com>
2023-02-06 16:07:00 +0200

    Add files via upload

---
+++ custom_modal_window/script.js
@@ -0,0 +1,13 @@
+const elgatoKeyLight = document.querySelector("#elgatoKeyLight");
+const elgatoKeyLightModal = document.querySelector("#elgatoKeyLightModal");
+const closeButton = document.querySelector(".close");
+
+const showModal = event => {
+    elgatoKeyLightModal.classList.add("show");
+};
+const closeModal = event => {
+    elgatoKeyLightModal.classList.remove("show");
+};
+
+elgatoKeyLight.addEventListener("click", showModal);
+closeButton.addEventListener("click", closeModal);
