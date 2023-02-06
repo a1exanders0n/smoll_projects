a56cb0bff2e95a90a89ac6c9d26e80cf8cbfba15
Max <82365842+a1exanders0n@users.noreply.github.com>
2023-02-06 14:12:39 +0000

    refactored code

--- custom_modal_window/script.js
+++ custom_modal_window/script.js
@@ -2,11 +2,11 @@ const elgatoKeyLight = document.querySelector("#elgatoKeyLight");
 const elgatoKeyLightModal = document.querySelector("#elgatoKeyLightModal");
 const closeButton = document.querySelector(".close");
 
-const showModal = event => {
-    elgatoKeyLightModal.classList.add("show");
+const showModal = (event) => {
+  elgatoKeyLightModal.classList.add("show");
 };
-const closeModal = event => {
-    elgatoKeyLightModal.classList.remove("show");
+const closeModal = (event) => {
+  elgatoKeyLightModal.classList.remove("show");
 };
 
 elgatoKeyLight.addEventListener("click", showModal);
